fbd3bcbd65c4b97274a00f08c096980669836362
Jakubsn23 <126696166+Jakubsn23@users.noreply.github.com>
2023-04-26 14:25:35 +0000

    4.4

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      System.out.println("1.Add new student:");
+      System.out.println("1.Add new student");
       System.out.println("2.Display student list");
       System.out.println("3.Quit");
       Scanner scanner = new Scanner(System.in);
@@ -22,11 +22,15 @@ class Main {
       switch(stud)
         {
           case 1: 
-            System.out.println("Enter student name:");
+            System.out.println("Enter student's name:");
             String name = scanner.next();
-            System.out.println("Enter student age:");
+            System.out.println("Enter student's age:");
             int age = scanner.nextInt();
-            s.addStudent(new Student (name, age));
+            System.out.println("Enter student's last name:");
+            String lastname = scanner.next();
+            System.out.println("Enter student's address:");
+            String address = scanner.next();
+            s.addStudent(new Student (name,lastname,address,age));
             break;
 
           case 2:
--- Student.java
+++ Student.java
@@ -1,24 +1,30 @@
 public class Student {
 
   private String Name;
+  private String Lastname;
   private int Age;
+  private String Address;
 
-  public Student(String name, int age) {
+  public Student(String name,String lastname,String address,int age) {
     Name = name;
     Age = age;
+    Address = address;
+    Lastname = lastname;
   }
 
   public String GetName() {return Name;}
+  public String GetLastname() {return Lastname;}
+  public String GetAddress() {return Address;}
   public int GetAge() {return Age;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + Lastname + " " + Address + " " + Integer.toString(Age);
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 4) 
+      return new Student("Parse Error","Parse Error","Parse Error", -1);
+    return new Student(data[0],data[1],data[2], Integer.parseInt(data[3]));
   }
 }
